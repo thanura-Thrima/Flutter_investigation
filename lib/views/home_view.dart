6dbfa88b46546e936d73ec883b82ad5a2dbad9a5
raveen thrimawithana <raveenthrimawithana111@gmail.com>
2023-11-07 01:41:17 +0200

    Adding async cardWidget generation

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -5,7 +5,6 @@ import 'package:test_app/widgets/card_widget.dart';
 
 class HomeView extends StatefulWidget {
   final String title;
-  final List<CardFile> mCards = CardRepository.getInstance().mCardList;
 
   HomeView({super.key, required this.title});
 
@@ -16,18 +15,39 @@ class HomeView extends StatefulWidget {
 }
 
 class HomeViewState extends State<HomeView> {
+  final Future<List<CardFile>> mCards = Future.delayed(
+      const Duration(seconds: 10),
+          () => CardRepository.getInstance()
+      .getCardList());
+
   HomeViewState();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: ListView.builder(
-          padding: const EdgeInsets.all(8),
-          itemCount: widget.mCards.length,
-          itemBuilder: (BuildContext context, int index) {
-            return CardWidget(
-                widget.mCards[index].mTitle, widget.mCards[index].mSubTitle);
-          }),
+      appBar: AppBar(title: const ListTile(
+        leading: Icon(Icons.home),
+        title: Text('Home'),
+      ), centerTitle: true,),
+      body: Center (
+        child: FutureBuilder<List<CardFile>>(
+        future: mCards,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+              itemCount: snapshot.data!.length,
+              prototypeItem: CardWidget(snapshot.data![0].mTitle,snapshot.data![0].mSubTitle),
+              itemBuilder: (context, index) {
+                return CardWidget(snapshot.data![0].mTitle,snapshot.data![0].mSubTitle);
+              },
+            );
+          }else{
+            return const Text('No data');
+          }
+        },
+      ),
+    ),
     );
   }
 }
+
